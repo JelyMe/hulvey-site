0f5ea80d27598f340d444e1f4bd8103b9ebf4bc7
JelyMe314 <JelyMe314@gmail.com>
2023-04-07 14:29:47 +1200

    thanks kev

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 async function task(i) {
     if (i < list.length - delays.length) await timer(100);
     else await timer(delays[i - (list.length - delays.length)]);
+    if (list[i] == "Christopher"){
+      text.style.display = "none";
+      img.style.display = "block";
+      img.src = "christopher1.png";
+      img.style.width = "230px";
+    }
+    else if(list[i] == "Coma"){
+      text.style.display = "none";
+      img.style.display = "block";
+      img.src = "coma1.png";
+      img.style.width = "230px";
+    }
+    else{
+    text.style.display = "block";
+    img.style.display = "none";
     text.textContent = list[i];
     text.style.fontFamily = fonts[i % 3];
     text.style.fontSize = fontSizes[i % 3];
+    }
   }
   function timer(ms) {
     return new Promise((res) => setTimeout(res, ms));
@@ -14,8 +30,8 @@ async function task(i) {
     }
     text.style.fontSize = "4em";
   }
-  var fonts = ["Poppins", "Bebas Neue", "Dancing Script"];
-  var fontSizes = ["1.5em", "2em", "3em"];
+  var fonts = ["Poppins", "Bebas Neue", "Canela"];
+  var fontSizes = ["1.5em", "2em", "2.5em"];
   var delays = [200, 300, 400];
   var list = [
     "WALK",
@@ -39,5 +55,6 @@ async function task(i) {
     "Hulvey"
   ];
   var text = document.getElementById("main-text");
+  var img = document.getElementById("main-img");
   
   main();
