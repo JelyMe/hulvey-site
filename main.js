76ad30f5a162463e78722f940e585c26c441be7e
JelyMe314 <JelyMe314@gmail.com>
2023-04-22 08:04:52 +1200

    Added dynmaic links

---
+++ main.js
@@ -0,0 +1,13 @@
+var music = document.getElementById("music");
+var store = document.getElementById("store");
+var game = document.getElementById("game");
+var tour = document.getElementById("tour");
+
+$.get('config.txt',{},function(content){
+    var links = content.split("/");
+    console.log(links);
+    if (links[0] != null && links[0] != "") music.href = links[0]; else music.parentElement.style.display = "none";
+    if (links[1] != null && links[1] != "") store.href = links[1]; else store.parentElement.style.display = "none";
+    if (links[2] != null && links[2] != "") game.href = links[2]; else game.parentElement.style.display = "none";
+    if (links[3] != null && links[3] != "") tour.href = links[3]; else tour.parentElement.style.display = "none";
+});
