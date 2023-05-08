7945e698deafa4880469bc1550d43380c562a684
JelyMe314 <JelyMe314@gmail.com>
2023-05-09 08:28:43 +1200

    desktop only game

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ $.get('config.txt',{},function(content){
     console.log(links);
     if (links[0] != null && links[0] != "") music.href = links[0]; else {music.parentElement.style.opacity = 0.4; music.parentElement.style.pointerEvents = "none";}
     if (links[1] != null && links[1] != "") store.href = links[1]; else {store.parentElement.style.opacity = 0.4; store.parentElement.style.pointerEvents = "none";}
-    if (links[2] != null && links[2] != "") game.href = links[2]; else {game.parentElement.style.opacity = 0.4; game.parentElement.style.pointerEvents = "none";}
+    if ((links[2] != null && links[2] != "") || !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) game.href = links[2]; else {game.parentElement.style.opacity = 0.4; game.parentElement.style.pointerEvents = "none";}
     if (links[3] != null && links[3] != "") tour.href = links[3]; else {tour.parentElement.style.opacity = 0.4; tour.parentElement.style.pointerEvents = "none";}
 });
