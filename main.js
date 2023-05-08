e9f6a98cdb5871064b025dd35c35515c9318be17
JelyMe314 <JelyMe314@gmail.com>
2023-05-09 09:06:12 +1200

    finally fixed the centering issue

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ var tour = document.getElementById("tour");
 $.get('config.txt',{},function(content){
     var links = content.split("*");
     console.log(links);
-    if (links[0] != null && links[0] != "") music.href = links[0]; else {music.parentElement.style.opacity = 0.4; music.parentElement.style.pointerEvents = "none";}
-    if (links[1] != null && links[1] != "") store.href = links[1]; else {store.parentElement.style.opacity = 0.4; store.parentElement.style.pointerEvents = "none";}
-    if ((links[2] != null && links[2] != "") && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) game.href = links[2]; else {game.parentElement.style.opacity = 0.4; game.parentElement.style.pointerEvents = "none";}
+    if (links[0] != null && links[0] != "") music.href = links[0]; else {music.parentElement.style.opacity = 0.4; music.parentElement.style.pointerEvents = "none"; if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) music.parentElement.style.display = "none"; }
+    if (links[1] != null && links[1] != "") store.href = links[1]; else {store.parentElement.style.opacity = 0.4; store.parentElement.style.pointerEvents = "none"; if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) store.parentElement.style.display = "none";}
+    if ((links[2] != null && links[2] != "") && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) game.href = links[2]; else {game.parentElement.style.opacity = 0.4; game.parentElement.style.pointerEvents = "none"; if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) game.parentElement.style.display = "none";}
     if (links[3] != null && links[3] != "") tour.href = links[3]; else {tour.parentElement.style.opacity = 0.4; tour.parentElement.style.pointerEvents = "none";}
 });
